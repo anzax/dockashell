9e3e9fb2cac95f738583ed1fa36bed6cd67d22ec
anzax <49080+anzax@users.noreply.github.com>
2025-05-28 20:49:15 +0200

    consolidate text utils

--- src/tui/ui-utils/input-utils.js
+++
@@ -1,7 +0,0 @@
-export const isEnterKey = (key) => !!(key && key.return);
-
-export const isBackKey = (input, key) =>
-  !!((key && key.escape) || input === 'q');
-
-export const isExitKey = (input, key) =>
-  isEnterKey(key) || isBackKey(input, key);
